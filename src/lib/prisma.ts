324319e951b4fbae2093eff12cdea47e42b6670b
jmherrerag <93355379+jherrerag20@users.noreply.github.com>
2025-12-01 23:15:34 -0600

    back to where we started

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,27 +1,11 @@
-// src/lib/prisma.ts
 import { PrismaClient } from "@prisma/client";
-import { PrismaPg } from "@prisma/adapter-pg";
 
-const connectionString = process.env.DATABASE_URL;
-if (!connectionString) {
-  throw new Error("DATABASE_URL no está definida");
-}
-
-const adapter = new PrismaPg({
-  connectionString,
-});
-
-const globalForPrisma = globalThis as unknown as {
-  prisma?: PrismaClient;
-};
+const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
 export const prisma =
-  globalForPrisma.prisma ??
+  globalForPrisma.prisma ||
   new PrismaClient({
-    adapter,
     log: ["error", "warn"],
   });
 
-if (process.env.NODE_ENV !== "production") {
-  globalForPrisma.prisma = prisma;
-}
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
